6a295e27b7dd9fe25bbda7f2ba6d5f2080c952ac
Liang Ding <d@b3log.org>
2018-10-12 14:10:35 +0800

    :rocket: 启动脚本

---
+++ forward-proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+proc_num=`ps -fe|grep '/root/go/src/github.com/b3log/forward-proxy/forward-proxy'|grep -v grep|wc -l`
+if [ $proc_num -gt 0 ]
+then
+  killall forward-proxy
+  echo 'killed forward-proxy'
+  sleep 1
+fi
+
+nohup /root/go/src/github.com/b3log/forward-proxy/forward-proxy > /var/log/forward-proxy/forward-proxy.log 2>&1 &
+
+echo 'forward-proxy done'
