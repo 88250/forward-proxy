783c4f868f0ead002255fb4c033fa34be27d0b3b
Liang Ding <d@b3log.org>
2018-10-12 14:13:44 +0800

    :rocket: 启动脚本

--- forward-proxy.sh
+++ forward-proxy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /root/go/src/github.com/b3log/forward-proxy/ && git checkout . && git pull
+
 proc_num=`ps -fe|grep '/root/go/src/github.com/b3log/forward-proxy/forward-proxy'|grep -v grep|wc -l`
 if [ $proc_num -gt 0 ]
 then
