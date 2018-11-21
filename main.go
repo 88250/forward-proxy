59c956f4e73df65310b165749f02f26ba000fb3c
Liang Ding <d@b3log.org>
2018-11-21 23:51:54 +0800

    :art: 日志

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	request := gorequest.New().Get(destURL).Timeout(10*time.Second).Retry(2, time.Second)
+	request := gorequest.New().Get(destURL).Timeout(10 * time.Second).Retry(2, time.Second)
 	for k, v := range r.Header {
 		request.Header.Set(k, fmt.Sprintf("%s", v))
 	}
@@ -82,10 +82,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	shortBody := ""
 	if 64 > len(responseBody) {
 		shortBody = responseBody
+		log.Printf("URL [%s], body [%s] ", responseData["URL"], string(retDataBytes))
 	} else {
 		shortBody = responseBody[:64]
 	}
-	log.Printf("ellapsed [%.1fs], length [%d], URL [%s], status [%d], %s",
+	log.Printf("ellapsed [%.1fs], length [%d], URL [%s], status [%d], body [%s]",
 		duration.Seconds(), len(retDataBytes), responseData["URL"], responseData["Status"], shortBody)
 }
 
