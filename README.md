d8c8d9f73d5dc42c8c249f3293b634d587c25dc8
Liang Ding <d@b3log.org>
2019-02-24 10:29:44 +0800

    :fire: 删除 AES 加密

--- README.md
+++ README.md
@@ -13,8 +13,7 @@
             "Connection": "close"
         }
     ], 
-    "method": "get", 
-    "key": "aes key"
+    "method": "get"
 }
 ```
 
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -86,11 +83,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	key := args["key"].(string)
-	if "" != key {
-		responseDataBytes = AESEncrypt(key, responseDataBytes)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(response.StatusCode)
 	w.Write(responseDataBytes)
@@ -110,41 +102,4 @@ func main() {
 	http.HandleFunc("/", handler)
 	logger.Info("Start serving on port 8888")
 	http.ListenAndServe(":8888", nil)
-}
-
-func AESEncrypt(key string, data []byte) []byte {
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		return nil
-	}
-	ecb := cipher.NewCBCEncrypter(block, []byte("RandomInitVector"))
-	content := data
-	content = PKCS5Padding(content, block.BlockSize())
-	crypted := make([]byte, len(content))
-	ecb.CryptBlocks(crypted, content)
-
-	return crypted
-}
-
-func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
-}
-
-func AESDecrypt(key string, crypt []byte) []byte {
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		return nil
-	}
-	ecb := cipher.NewCBCDecrypter(block, []byte("RandomInitVector"))
-	decrypted := make([]byte, len(crypt))
-	ecb.CryptBlocks(decrypted, crypt)
-
-	return PKCS5Trimming(decrypted)
-}
-
-func PKCS5Trimming(encrypt []byte) []byte {
-	padding := encrypt[len(encrypt)-1]
-	return encrypt[:len(encrypt)-int(padding)]
 }
